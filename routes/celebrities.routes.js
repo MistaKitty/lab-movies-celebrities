a95999f061a4fc5a3768da36149e64df6b9dde63
MistaKitty <tiago.abreu.laibacas@gmail.com>
2024-06-15 11:48:26 +0100

    Added the form for creating celebrities 💄📝

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
   res.send('List of celebrities');
 });
 
+router.get('/create', (req, res) => {
+    res.send('form of celebrities');
+  });
+
 module.exports = router;
